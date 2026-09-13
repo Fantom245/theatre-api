ffcbd5a61343c51bf373f94600976de2697e6f77
Fantom <gyrkalomas@gmail.com>
2026-09-14 01:24:13 +0300

    create base model for play app

--- play/models.py
+++ play/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ["last_name", "first_name"]
+        verbose_name = "Actor"
+        verbose_name_plural = "Actors"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Genre"
+        verbose_name_plural = "Genres"
+
+    def __str__(self):
+        return self.name
+
+
+class Play(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+    actors = models.ManyToManyField(Actor, related_name="plays", blank=True)
+    genres = models.ManyToManyField(Genre, related_name="plays", blank=True)
+
+    class Meta:
+        ordering = ["title"]
+        verbose_name = "Play"
+        verbose_name_plural = "Plays"
+
+    def __str__(self):
+        return self.title
