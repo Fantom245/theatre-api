82c00a1e3a5a81250a7391dcffa70fd3a752a379
Fantom <gyrkalomas@gmail.com>
2026-09-20 17:01:51 +0300

    create templates and base.html

--- theater_api/settings.py
+++ theater_api/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'theater_api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
